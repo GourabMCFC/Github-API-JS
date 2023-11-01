d00e6e80aeaa13e8e2425a6d50091358a1fa78c3
GourabMCFC <gourab.mukherjee.fiem.cse20@teamfuture.in>
2023-11-02 00:40:49 +0530

    Done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Query Selectors 
 let card = document.querySelector("section").firstElementChild;
 let image = card.firstElementChild;
-let [c1, c2, c3] = card.lastElementChild.children;
+let [c1, c2, c3, c4] = card.lastElementChild.children;
 // API Requests
 let output = null;
 let url = `https://api.github.com/users/hiteshchoudhary`;
@@ -11,11 +11,24 @@ const imageDiv = ({ html_url, avatar_url }) => {
     image.setAttribute("target", "_blank"); //LinkSetup
     image.firstElementChild.setAttribute("src", avatar_url);//Image Update
 }
-const contentDiv = ({ name, bio, html_url }) => {
-    c1.setAttribute("href",html_url);
-    c1.setAttribute("target","_blank");
+const addContent = (type, val) => {
+    let header = document.createElement("h5");
+    header.setAttribute("class", "dynamic");
+    header.appendChild(document.createTextNode(`${type.toUpperCase()} : ${val}`));
+    c4.appendChild(header);
+}
+const contentDiv = ({ name, bio, html_url, followers, created_at }) => {
+    c1.setAttribute("href", html_url);
+    c1.setAttribute("target", "_blank");
     c1.lastElementChild.firstChild.replaceWith(document.createTextNode(name));
     c2.firstChild.replaceWith(document.createTextNode(bio));
+    c3.addEventListener("click", e => {
+        e.preventDefault();
+        c4.innerHTML = "";
+        addContent("followers",followers);
+        addContent("started",new Date(created_at).toLocaleDateString());
+        // console.log();
+    })
 }
 
 // Request
@@ -25,7 +38,6 @@ xhr.send();
 xhr.onreadystatechange = () => {
     if (xhr.readyState === 4) {
         output = JSON.parse(xhr.responseText);
-        // console.log(output);
         imageDiv(output);
         contentDiv(output);
     }
